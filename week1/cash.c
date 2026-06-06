c8a382d975e301a29d5254c0c011f2ad06ccb25f
ShivamMehla <shivambhaini04@gmail.com>
2026-06-06 19:44:46 +0530

    Update cash.c

--- week1/cash.c
+++ week1/cash.c
@@ -1,1 +1,37 @@
+#include <cs50.h>
+#include <stdio.h>
+int main(void)
+
+{
+   int change_owed;
+   do
+   {
+      change_owed = get_int("change owed : ");
+   }
+   while (change_owed < 0);
+
+   if (change_owed == 0)
+   {
+      printf("0\n");
+      return 0;
+   }
+
+    int quarter = change_owed / 25;
+    printf("%i\n", quarter);
+    change_owed %= 25;
+
+    int dimes = change_owed / 10;
+    printf("%i\n", dimes);
+    change_owed %= 10;
+
+    int nickels = change_owed / 5;
+    printf("%i\n", nickels);
+    change_owed %= 5;
+
+    int pennies = change_owed;
+    printf("%i\n", pennies);
+
+    printf("total : %i\n", quarter + dimes + nickels + pennies);
+
+}
 
