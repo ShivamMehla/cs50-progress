c44191be71d322e39abd0d94d2f600d4c1203686
ShivamMehla <shivambhaini04@gmail.com>
2026-06-06 19:46:27 +0530

    Update mario_more.c

--- week1/mario_more.c
+++ week1/mario_more.c
@@ -1,1 +1,46 @@
+#include <cs50.h>
+#include <stdio.h>
+void print_row(int brick, int n);
+void print_row2(int brick);
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Height : ");
+    }
+    while(n < 1 || n > 8);
+    for(int i = 0; i < n; i++)
+    {
+        print_row(i + 1, n);
+        printf("  ");
+        print_row2(i + 1);
+    }
+}
+
+
+
+void print_row(int brick, int n)
+{
+    for(int i = 0; i < n - brick; i++)
+    {
+        printf(" ");
+    }
+    for(int i = 0; i < brick; i++)
+    {
+        printf("#");
+    }
+}
+
+void print_row2(int brick)
+{
+    for(int i = 0; i < brick; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
+
+
+
 
